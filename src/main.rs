aa5e91841182c6ea73af78a2c1423af92f44217c
Ethan Thoma <ethanthoma@gmail.com>
2026-06-06 15:38:59 -0700

    run: concurrent blink

--- src/main.rs
+++ src/main.rs
@@ -40,6 +40,15 @@ async fn task_led(mut led: gpio::Output<'static>) {
     }
 }
 
+#[embassy_executor::task]
+async fn task_blink(mut led: gpio::Output<'static>) {
+    loop {
+        led.toggle();
+        info!("blink = {}", led.get_output_level());
+        Timer::after_secs(1).await;
+    }
+}
+
 #[embassy_executor::main]
 async fn main(spawner: Spawner) -> ! {
     let p = embassy_stm32::init(Default::default());
@@ -47,9 +56,11 @@ async fn main(spawner: Spawner) -> ! {
 
     let button = exti::ExtiInput::new(p.PA0, p.EXTI0, gpio::Pull::Down, Irqs);
     let led_green = gpio::Output::new(p.PD12, gpio::Level::Low, gpio::Speed::Low);
+    let led_red = gpio::Output::new(p.PD14, gpio::Level::Low, gpio::Speed::Low);
 
     spawner.spawn(unwrap!(task_button(button)));
     spawner.spawn(unwrap!(task_led(led_green)));
+    spawner.spawn(unwrap!(task_blink(led_red)));
 
     loop {
         Timer::after_secs(1).await;
