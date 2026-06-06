9df32d5f812615fd8975e749218a234555ba020c
Ethan Thoma <ethanthoma@gmail.com>
2026-06-06 11:44:33 -0700

    run: toggle light state on press

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ use defmt::info;
 use embassy_executor::Spawner;
 use embassy_stm32::exti::ExtiInput;
 use embassy_stm32::gpio::{Input, Level, Output, Pull, Speed};
+use embassy_time::Timer;
 use {defmt_rtt as _, panic_probe as _};
 
 #[embassy_executor::main]
@@ -16,13 +17,16 @@ async fn main(_spawner: Spawner) -> ! {
 
     let i0 = Input::new(p.PA0, Pull::Down);
     let mut user_button = ExtiInput::new(i0, p.EXTI0);
+    let mut on = false;
 
     loop {
         user_button.wait_for_rising_edge().await;
-        info!("pressed!");
-        green.set_high();
-
+        on = !on;
+        let level = if on { Level::High } else { Level::Low };
+        green.set_level(level);
+        info!("led = {}", on);
+        Timer::after_millis(20).await;
         user_button.wait_for_falling_edge().await;
-        green.set_low();
+        Timer::after_millis(20).await;
     }
 }
