35efb4356e02409f83054cb4300b8b20cfe10f28
Ethan Thoma <ethanthoma@gmail.com>
2026-06-06 13:56:10 -0700

    deps: upgrade

--- src/main.rs
+++ src/main.rs
@@ -3,26 +3,35 @@
 
 use defmt::info;
 use embassy_executor::Spawner;
-use embassy_stm32::exti::ExtiInput;
-use embassy_stm32::gpio::{Input, Level, Output, Pull, Speed};
+use embassy_stm32::bind_interrupts;
+use embassy_stm32::exti;
+use embassy_stm32::gpio;
+use embassy_stm32::interrupt;
 use embassy_time::Timer;
 use {defmt_rtt as _, panic_probe as _};
+
+bind_interrupts!(struct Irqs {
+    EXTI0 => exti::InterruptHandler<interrupt::typelevel::EXTI0>;
+});
 
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) -> ! {
     let p = embassy_stm32::init(Default::default());
     info!("starting...");
 
-    let mut green = Output::new(p.PD12, Level::Low, Speed::Low);
+    let mut green = gpio::Output::new(p.PD12, gpio::Level::Low, gpio::Speed::Low);
 
-    let i0 = Input::new(p.PA0, Pull::Down);
-    let mut user_button = ExtiInput::new(i0, p.EXTI0);
+    let mut user_button = exti::ExtiInput::new(p.PA0, p.EXTI0, gpio::Pull::Down, Irqs);
     let mut on = false;
 
     loop {
         user_button.wait_for_rising_edge().await;
         on = !on;
-        let level = if on { Level::High } else { Level::Low };
+        let level = if on {
+            gpio::Level::High
+        } else {
+            gpio::Level::Low
+        };
         green.set_level(level);
         info!("led = {}", on);
         Timer::after_millis(20).await;
