8bcacb1fb7fa0c685bf30ca7bc52f8e456b24c6b
Ethan Thoma <ethanthoma@gmail.com>
2026-06-06 11:25:24 -0700

    run: toggle led with user btn

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,8 @@
 
 use defmt::info;
 use embassy_executor::Spawner;
-use embassy_stm32::gpio::{Level, Output, Speed};
-use embassy_time::Timer;
+use embassy_stm32::exti::ExtiInput;
+use embassy_stm32::gpio::{Input, Level, Output, Pull, Speed};
 use {defmt_rtt as _, panic_probe as _};
 
 #[embassy_executor::main]
@@ -12,18 +12,17 @@ async fn main(_spawner: Spawner) -> ! {
     let p = embassy_stm32::init(Default::default());
     info!("starting...");
 
-    let mut green = Output::new(p.PD12, Level::High, Speed::Low);
-    let mut red = Output::new(p.PD14, Level::High, Speed::Low);
+    let mut green = Output::new(p.PD12, Level::Low, Speed::Low);
+
+    let i0 = Input::new(p.PA0, Pull::Down);
+    let mut user_button = ExtiInput::new(i0, p.EXTI0);
 
     loop {
-        red.set_low();
-        info!("green");
+        user_button.wait_for_rising_edge().await;
+        info!("pressed!");
         green.set_high();
-        Timer::after_secs(1).await;
 
+        user_button.wait_for_falling_edge().await;
         green.set_low();
-        info!("red");
-        red.set_high();
-        Timer::after_secs(1).await;
     }
 }
