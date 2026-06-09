ff3f466fdcb7669d686a20a005347ff094077e3f
Ethan Thoma <ethanthoma@gmail.com>
2026-06-09 16:18:26 -0700

    run: mini PID emulation + supervisor

--- src/main.rs
+++ src/main.rs
@@ -16,6 +16,9 @@ use embassy_stm32::time;
 use embassy_stm32::timer::simple_pwm;
 use embassy_sync::blocking_mutex::raw::ThreadModeRawMutex;
 use embassy_sync::channel::Channel;
+use embassy_sync::signal::Signal;
+use embassy_time::Duration;
+use embassy_time::Ticker;
 use embassy_time::Timer;
 use {defmt_rtt as _, panic_probe as _};
 
@@ -34,15 +37,6 @@ async fn task_button(mut button: exti::ExtiInput<'static, Async>) {
         Timer::after_millis(20).await;
         button.wait_for_falling_edge().await;
         Timer::after_millis(20).await;
-    }
-}
-
-#[embassy_executor::task]
-async fn task_led(mut led: gpio::Output<'static>) {
-    loop {
-        CHANNEL.receive().await;
-        led.toggle();
-        info!("led = {}", led.get_output_level())
     }
 }
 
@@ -93,6 +87,112 @@ async fn task_temp(mut adc: Adc<'static, ADC1>) {
     }
 }
 
+static SETPOINT: Signal<ThreadModeRawMutex, f32> = Signal::new();
+static TARGET: Signal<ThreadModeRawMutex, f32> = Signal::new();
+
+enum Joint {
+    Home,
+    Extended,
+}
+
+#[embassy_executor::task]
+async fn task_control() {
+    const HZ: u64 = 100;
+    let mut ticker = Ticker::every(Duration::from_hz(HZ));
+    let mut ticks = 1;
+
+    const K: u32 = 100;
+    const K_P: f32 = 0.1;
+    const K_I: f32 = 0.1;
+    const K_D: f32 = 0.01;
+
+    let dt: f32 = 1. / HZ as f32;
+    let tau: f32 = 2.0;
+
+    let mut v = 0.0;
+    let mut u;
+    let mut integral = 0.0;
+    let mut sp = 0.0;
+    let mut v_prev = 0.;
+    loop {
+        ticker.next().await;
+
+        if let Some(new_sp) = SETPOINT.try_take() {
+            sp = new_sp;
+        }
+
+        if ticks >= 5 {
+            info!("sp = {}; v = {}", sp, v);
+            ticks = 0;
+        }
+
+        let e = sp - v;
+        let p = K_P * e;
+        let i = K_I * integral;
+        let d = -K_D * (v - v_prev) / dt;
+        let u_raw = p + i + d;
+        u = u_raw.clamp(-1.0, 1.0);
+
+        if u == u_raw {
+            integral += e * dt;
+        }
+        v_prev = v;
+
+        v += (K as f32 * u - v) * (dt / tau);
+
+        ticks += 1;
+    }
+}
+
+#[embassy_executor::task]
+async fn task_motion() {
+    const HZ: u64 = 100; // hz
+    let mut ticker = Ticker::every(Duration::from_hz(HZ));
+
+    const A_MAX: f32 = 20.0;
+
+    let dt: f32 = 1. / HZ as f32;
+
+    let mut target = 0.;
+    let mut sp = 0.;
+    loop {
+        ticker.next().await;
+
+        if let Some(new_target) = TARGET.try_take() {
+            target = new_target;
+        }
+
+        let step = A_MAX * dt;
+        if sp < target {
+            sp = (sp + step).min(target);
+        } else if sp > target {
+            sp = (sp - step).max(target);
+        }
+        SETPOINT.signal(sp);
+    }
+}
+
+#[embassy_executor::task]
+async fn task_supervisor(mut led: gpio::Output<'static>) {
+    let mut state = Joint::Home;
+    loop {
+        CHANNEL.receive().await;
+
+        match state {
+            Joint::Home => {
+                state = Joint::Extended;
+                led.set_high();
+                TARGET.signal(50.);
+            }
+            Joint::Extended => {
+                led.set_low();
+                state = Joint::Home;
+                TARGET.signal(0.);
+            }
+        }
+    }
+}
+
 #[embassy_executor::main]
 async fn main(spawner: Spawner) {
     let p = embassy_stm32::init(Default::default());
@@ -116,8 +216,10 @@ async fn main(spawner: Spawner) {
     let adc = Adc::new(p.ADC1);
 
     spawner.spawn(unwrap!(task_button(button)));
-    spawner.spawn(unwrap!(task_led(led_green)));
     spawner.spawn(unwrap!(task_blink(led_red)));
     spawner.spawn(unwrap!(task_breathe(pwm)));
     spawner.spawn(unwrap!(task_temp(adc)));
+    spawner.spawn(unwrap!(task_control()));
+    spawner.spawn(unwrap!(task_motion()));
+    spawner.spawn(unwrap!(task_supervisor(led_green)));
 }
